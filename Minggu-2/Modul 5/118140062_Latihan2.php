ac195ccbe8952b26837fc02c785827a6dac3890e
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2020-11-15 00:11:17 +0700

    update sorting using sort function

--- Minggu-2/Modul 5/118140062_Latihan2.php
+++ Minggu-2/Modul 5/118140062_Latihan2.php
@@ -1,28 +1,6 @@
 <?php
     $arr = ["lanirne", "aduh", "qifuat", "toda", "anevi", "samid", "kifuat"];
-
-    for ($i=0; $i<count($arr); $i++){
-        $target = $i;
-        for ($j=$i+1; $j<count($arr); $j++){
-            $shorter = strlen($arr[$i]);
-
-            if (strlen($arr[$i])>strlen($arr[$j])){
-                $shorter = strlen($arr[$j]);
-            }
-            for ($k=0; $k<$shorter; $k++){
-                if ($arr[$j][$k]<$arr[$target][$k]){
-                    $target = $j;
-                    break;
-                }else if ($arr[$j][$k]>$arr[$target][$k]){
-                    break;
-                }
-            }  
-        }
-        $temp = $arr[$i];
-        $arr[$i] = $arr[$target];
-        $arr[$target] = $temp;
-    }
-
+    sort($arr);
     echo "Hasil pengurutan : [";
     for($i = 0;$i<count($arr);$i++){
         if($i!=count($arr)-1){
